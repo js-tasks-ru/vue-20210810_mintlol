5cb039fb7f60a24ce66c54837dee59362b26223b
laniakea42 <mintlol@mail.ru>
2021-10-11 18:07:37 +0600

    solve Components and VBindCover

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { fetchMeetupById } from './meetupService.js';
+import MeetupView from '../06-MeetupView/MeetupView.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,57 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    }
+  },
+
+  data() {
+    return {
+      meetup: '',
+      meetupLoaded: false,
+      meetupError: undefined,
+    }
+  },
+
+  watch: {
+    meetupId(newValue) {
+      this.updateMeetup(newValue)
+    }
+  },
+
+  methods: {
+    updateMeetup(newValue) {
+      this.meetupLoaded = false;
+
+      fetchMeetupById(newValue)
+        .then((meetup) => {
+        this.meetup = meetup;
+        this.meetupLoaded = true;
+        this.meetupError = undefined;
+        })
+        .catch((e) => {
+          this.meetupLoaded = false;
+          this.meetupError = e.message;
+        })
+    }
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <meetup-view v-if="meetupLoaded" :meetup="meetup"></meetup-view>
 
-      <ui-container>
+      <ui-container v-if="!meetupLoaded && !meetupError">
         <ui-alert>Загрузка...</ui-alert>
       </ui-container>
 
-      <ui-container>
-        <ui-alert>error</ui-alert>
+      <ui-container v-if="meetupError">
+        <ui-alert> {{meetupError}} </ui-alert>
       </ui-container>
     </div>`,
 });
